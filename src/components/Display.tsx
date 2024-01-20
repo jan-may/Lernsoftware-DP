81e715f0dd2ff4ebcde16d0e383e22347adf02d8
Jan May <jan_may@gmx.net>
2024-01-20 13:25:29 +0100

    Refactor WriteUpButton component to display multiple buttons

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -10,7 +10,7 @@ export const Display = () => {
   const { selectedProblem } = useAppSelector((store) => store.settings);
   return (
     <div className="relative">
-      <WriteUpButton />
+      {activeButton !== ActivButton.problem ? <WriteUpButton /> : null}
       <Card className="p-2 m-2 max-h-[calc(100vh-78px)]">
         {activeButton === ActivButton.bottomUp ? (
           <Grid />
--- src/components/WriteUpButton.tsx
+++ src/components/WriteUpButton.tsx
@@ -1,4 +1,4 @@
-import { HelpCircle } from "lucide-react";
+import { HelpCircle, Gauge } from "lucide-react";
 import { Button } from "./ui/button";
 import {
   Tooltip,
@@ -7,19 +7,34 @@ import {
   TooltipTrigger,
 } from "../components/ui/tooltip";
 
+const Buttons = [
+  {
+    icon: <Gauge />,
+    text: "Laufzeitanalyse anzeigen",
+  },
+  {
+    icon: <HelpCircle />,
+    text: "Erklärung anzeigen",
+  },
+];
+
 export const WriteUpButton = () => {
   return (
-    <TooltipProvider delayDuration={250}>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant="secondary" className="absolute bottom-1 right-2">
-            <HelpCircle />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Erklärung anzeigen</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="absolute bottom-1 right-2">
+      {Buttons.map((button: any) => (
+        <TooltipProvider delayDuration={250} key={button.id}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="secondary" size="sm">
+                {button.icon}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{button.text}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ))}
+    </div>
   );
 };
