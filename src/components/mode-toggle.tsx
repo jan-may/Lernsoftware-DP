3f0ecf560c0651cc98c8934b9e9733f0b1fabe38
Jan May <jan_may@gmx.net>
2024-02-12 10:56:12 +0100

    remove system theme from toggle and set it as default internaly

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -28,12 +28,6 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
